c28f0f97b90d00da03938d14caacbccde4f7458a
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-02-22 23:01:58 +0200

    Explorer Top Book & Carousel components

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import "./App.css";
+import { Carousel } from "./layouts/HomePage/Carousel";
+import { ExploreTopBooks } from "./layouts/HomePage/ExploreTopBooks";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 
 function App() {
-  return <Navbar />;
+  return (
+    <div>
+      <Navbar />
+      <ExploreTopBooks />
+      <Carousel />
+    </div>
+  );
 }
 
 export default App;
