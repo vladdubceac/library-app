8ac726881b88fab315266b1c9e5f2096df7bfca7
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-02-26 17:15:50 +0200

    Configure READ only API

---
+++ 02-backend/spring-boot-library/src/main/java/com/vladdubceac/springbootlibrary/config/MyDataRestConfig.java
@@ -0,0 +1,36 @@
+package com.vladdubceac.springbootlibrary.config;
+
+import com.vladdubceac.springbootlibrary.entity.Book;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private String theAllowedOrigins = "http://localhost:3000";
+
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors){
+        HttpMethod[]      theUnsupportedActions = {HttpMethod.POST,
+        HttpMethod.PATCH, HttpMethod.DELETE,HttpMethod.PUT};
+
+        config.exposeIdsFor(Book.class);
+
+        disableHttpMethods(Book.class, config, theUnsupportedActions);
+
+        /* Configure CORS Mapping */
+        cors.addMapping(config.getBasePath() + "/**")
+                .allowedOrigins(theAllowedOrigins);
+    }
+
+    private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions){
+        config.getExposureConfiguration()
+                .forDomainType(theClass)
+                .withItemExposure((metadata, httpMethods) ->
+                        httpMethods.disable(theUnsupportedActions))
+         .withCollectionExposure((metadata, httpMethods)->
+                 httpMethods.disable(theUnsupportedActions));
+    }
+}
