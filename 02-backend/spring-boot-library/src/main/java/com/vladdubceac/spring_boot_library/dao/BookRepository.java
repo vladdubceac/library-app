e374655c5d7d60236108620c3a9f062c35dbb2c8
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-03-28 22:50:21 +0300

    Added 'find by title' functionality

--- 02-backend/spring-boot-library/src/main/java/com/vladdubceac/spring_boot_library/dao/BookRepository.java
+++ 02-backend/spring-boot-library/src/main/java/com/vladdubceac/spring_boot_library/dao/BookRepository.java
@@ -1,7 +1,12 @@
 package com.vladdubceac.spring_boot_library.dao;
 
 import com.vladdubceac.spring_boot_library.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Page<Book> findByTitleContaining(@Param("title") String title, Pageable pageable);
 }
--- 03-frontend/react-library/src/layouts/SearchBooksPage/SearchBooksPage.tsx
+++ 03-frontend/react-library/src/layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -13,14 +13,20 @@ export const SearchBooksPage = () => {
   const [booksPerPage] = useState(5);
   const [totalAmountOfBooks, setTotalAmountOfBooks] = useState(0);
   const [totalPages, setTotalPages] = useState(0);
+  const [search, setSearch] = useState("");
+  const [searchUrl, setSearchUrl] = useState("");
 
   useEffect(() => {
     const fetchBooks = async () => {
       const baseUrl: string = "http://localhost:8080/api/books";
 
-      const url: string = `${baseUrl}?page=${
-        currentPage - 1
-      }&size=${booksPerPage}`;
+      let url: string = "";
+
+      if (searchUrl === "") {
+        url = `${baseUrl}?page=${currentPage - 1}&size=${booksPerPage}`;
+      } else {
+        url = baseUrl + searchUrl;
+      }
 
       const response = await fetch(url);
 
@@ -59,7 +65,7 @@ export const SearchBooksPage = () => {
       setHttpError(error.message);
     });
     window.scrollTo(0, 0);
-  }, [currentPage]);
+  }, [currentPage, searchUrl]);
 
   if (isLoading) {
     return <SpinnerLoading />;
@@ -72,6 +78,16 @@ export const SearchBooksPage = () => {
       </div>
     );
   }
+
+  const searchHandleChange = () => {
+    if (search === "") {
+      setSearchUrl("");
+    } else {
+      setSearchUrl(
+        `/search/findByTitleContaining?title=${search}&page=0&size=${booksPerPage}`
+      );
+    }
+  };
 
   const indexOfLastBook: number = currentPage * booksPerPage;
   const indexOfFirstBook: number = indexOfLastBook - booksPerPage;
@@ -93,8 +109,14 @@ export const SearchBooksPage = () => {
                 type="search"
                 placeholder="Search"
                 aria-labelledby="Search"
+                onChange={(e) => setSearch(e.target.value)}
               />
-              <button className="btn btn-outline-success">Search</button>
+              <button
+                className="btn btn-outline-success"
+                onClick={() => searchHandleChange()}
+              >
+                Search
+              </button>
             </div>
           </div>
           <div className="col-4">
@@ -141,15 +163,31 @@ export const SearchBooksPage = () => {
             </div>
           </div>
         </div>
-        <div className="mt-3">
-          <h5>Number of results : ({totalAmountOfBooks})</h5>
-        </div>
-        <p>
-          {indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks} items:
-        </p>
-        {books.map((book) => (
-          <SearchBook book={book} key={book.id} />
-        ))}
+        {totalAmountOfBooks > 0 ? (
+          <>
+            <div className="mt-3">
+              <h5>Number of results : ({totalAmountOfBooks})</h5>
+            </div>
+            <p>
+              {indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks}{" "}
+              items:
+            </p>
+            {books.map((book) => (
+              <SearchBook book={book} key={book.id} />
+            ))}
+          </>
+        ) : (
+          <div className="m-5">
+            <h3>Can't find what you are looking for ?</h3>
+            <a
+              type="button"
+              className="btn main-color btn-md px-4 me-md-2 fw-bold text-white"
+              href="#"
+            >
+              Library Services
+            </a>
+          </div>
+        )}
         {totalPages > 1 && (
           <Pagination
             currentPage={currentPage}
