aceaea4a3eda4f46a785fe900c56e3369e2a4fe8
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-03-28 23:53:20 +0300

    Added 'search by category' functionality

--- 02-backend/spring-boot-library/src/main/java/com/vladdubceac/spring_boot_library/dao/BookRepository.java
+++ 02-backend/spring-boot-library/src/main/java/com/vladdubceac/spring_boot_library/dao/BookRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     Page<Book> findByTitleContaining(@Param("title") String title, Pageable pageable);
+
+    Page<Book> findByCategory(@Param("category") String category, Pageable pageable);
 }
