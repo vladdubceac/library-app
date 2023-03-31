16ee15023df3ba875ef1ee718fb75fe8a6280d8e
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-03-31 23:32:08 +0300

    Added reviews stars on checkout page

--- 02-backend/spring-boot-library/src/main/java/com/vladdubceac/spring_boot_library/config/MyDataRestConfig.java
+++ 02-backend/spring-boot-library/src/main/java/com/vladdubceac/spring_boot_library/config/MyDataRestConfig.java
@@ -1,6 +1,7 @@
 package com.vladdubceac.spring_boot_library.config;
 
 import com.vladdubceac.spring_boot_library.entity.Book;
+import com.vladdubceac.spring_boot_library.entity.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -12,25 +13,27 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
     private String theAllowedOrigins = "http://localhost:3000";
 
-    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors){
-        HttpMethod[]      theUnsupportedActions = {HttpMethod.POST,
-        HttpMethod.PATCH, HttpMethod.DELETE,HttpMethod.PUT};
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.POST,
+                HttpMethod.PATCH, HttpMethod.DELETE, HttpMethod.PUT};
 
         config.exposeIdsFor(Book.class);
+        config.exposeIdsFor(Review.class);
 
         disableHttpMethods(Book.class, config, theUnsupportedActions);
+        disableHttpMethods(Review.class, config, theUnsupportedActions);
 
         /* Configure CORS Mapping */
         cors.addMapping(config.getBasePath() + "/**")
                 .allowedOrigins(theAllowedOrigins);
     }
 
-    private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions){
+    private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
         config.getExposureConfiguration()
                 .forDomainType(theClass)
                 .withItemExposure((metadata, httpMethods) ->
                         httpMethods.disable(theUnsupportedActions))
-         .withCollectionExposure((metadata, httpMethods)->
-                 httpMethods.disable(theUnsupportedActions));
+                .withCollectionExposure((metadata, httpMethods) ->
+                        httpMethods.disable(theUnsupportedActions));
     }
 }
---
+++ 02-backend/spring-boot-library/src/main/java/com/vladdubceac/spring_boot_library/dao/ReviewRepository.java
@@ -0,0 +1,13 @@
+package com.vladdubceac.spring_boot_library.dao;
+
+import com.vladdubceac.spring_boot_library.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Page<Review> findByBookId(@Param("bookId") Long bookId, Pageable pageable);
+
+}
