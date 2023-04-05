8103a014290c2901844a53cd9eefcb554a51ee23
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-04-05 08:39:16 +0300

    Added access token and security configurations

--- 02-backend/spring-boot-library/src/main/java/com/vladdubceac/spring_boot_library/controller/BookController.java
+++ 02-backend/spring-boot-library/src/main/java/com/vladdubceac/spring_boot_library/controller/BookController.java
@@ -2,6 +2,7 @@ package com.vladdubceac.spring_boot_library.controller;
 
 import com.vladdubceac.spring_boot_library.entity.Book;
 import com.vladdubceac.spring_boot_library.service.BookService;
+import com.vladdubceac.spring_boot_library.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,20 +19,20 @@ public class BookController {
     }
 
     @GetMapping("/secure/currentloans/count")
-    public int currentLoansCount(){
-        String userEmail = "testuser@email.com";
+    public int currentLoansCount(@RequestHeader(value="Authorization") String token){
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, ExtractJWT.EMAIL_EXTRACTION_KEY);
         return bookService.currentLoansCount(userEmail);
     }
 
     @GetMapping("/secure/ischeckedout/byuser")
-    public Boolean checkoutBookByUser(@RequestParam Long bookId){
-        String userEmail = "testuser@email.com";
+    public Boolean checkoutBookByUser(@RequestHeader(value="Authorization") String token, @RequestParam Long bookId){
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, ExtractJWT.EMAIL_EXTRACTION_KEY);
         return bookService.checkoutBookByUser(userEmail,bookId);
     }
 
     @PutMapping("/secure/checkout")
-    public Book checkoutBook(@RequestParam Long bookId) throws Exception {
-        String userEmail = "testuser@email.com";
+    public Book checkoutBook(@RequestHeader(value="Authorization") String token, @RequestParam Long bookId) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, ExtractJWT.EMAIL_EXTRACTION_KEY);
         return bookService.checkoutBook(userEmail,bookId);
     }
 }
---
+++ 02-backend/spring-boot-library/src/main/java/com/vladdubceac/spring_boot_library/utils/ExtractJWT.java
@@ -0,0 +1,45 @@
+package com.vladdubceac.spring_boot_library.utils;
+
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
+public final class ExtractJWT {
+
+    public static final String EMAIL_EXTRACTION_KEY = "\"sub\"";
+
+    private ExtractJWT(){
+    }
+
+    public static String payloadJWTExtraction(String token, String extraction){
+        token.replace("Bearer","");
+
+        String[] chunks = token.split("\\.");
+        Base64.Decoder decoder = Base64.getUrlDecoder();
+
+        String payload = new String(decoder.decode(chunks[1]));
+
+        String[] entries = payload.split(",");
+
+        Map<String,String> map = new HashMap<String, String>();
+
+        for(String entry : entries) {
+            String[] keyValue = entry.split(":");
+            if(keyValue[0].equals(extraction)){
+                int remove = 1;
+                if(keyValue[1].endsWith("}")){
+                    remove = 2;
+                }
+                keyValue[1] = keyValue[1].substring(0, keyValue[1].length() - remove);
+                keyValue[1] = keyValue[1].substring(1);
+
+                map.put(keyValue[0],keyValue[1]);
+            }
+        }
+
+        if(map.containsKey(extraction)){
+            return map.get(extraction);
+        }
+        return null;
+    }
+}
